ba8f64416781556534f8ebf1a763050cfb26197b
CAMHub PublishBot <octravis@us.ibm.com>
2022-05-10 03:35:09 +0000

    Automated CAMHub::publish on 2022-05-10 03:35:09 UTC

---
+++ terraform100/IKS/mcm-klusterlet/main.tf
@@ -0,0 +1,51 @@
+resource "random_string" "random-dir" {
+  length  = 8
+  special = false
+}
+
+module "cluster-credentials" {
+  source  = "git::https://github.com/IBM-CAMHub-Open/template_rhacm_modules.git//cluster_credentials?ref=5.2.0"
+
+  cluster_type   = "iks"
+  work_directory = "mcm${random_string.random-dir.result}"
+
+  ## Details for accessing the target cluster
+  cluster_name                  = var.cluster_name
+  cluster_config                = var.cluster_config
+  cluster_certificate_authority = var.cluster_certificate_authority
+
+  ## Access to optional bastion host
+  bastion_host        = var.bastion_host
+  bastion_user        = var.bastion_user
+  bastion_private_key = var.bastion_private_key
+  bastion_port        = var.bastion_port
+  bastion_host_key    = var.bastion_host_key
+  bastion_password    = var.bastion_password
+}
+
+module "cluster-import" {
+  source  = "git::https://github.com/IBM-CAMHub-Open/template_rhacm_modules.git//cluster_import?ref=5.2.0"
+
+  dependsOn      = module.cluster-credentials.credentials_generated
+  work_directory = "mcm${random_string.random-dir.result}"
+
+  ## Details for accessing the RHACM hub-cluster
+  ocp_api_endpoint  = var.ocp_api_endpoint
+  ocp_user          = var.ocp_user
+  ocp_password      = var.ocp_password
+  ocp_ca_cert       = length(var.ocp_ca_cert) > 0 ? base64decode(var.ocp_ca_cert) : var.ocp_ca_cert
+  ocp_token         = var.ocp_token
+  oc_cli_endpoint   = var.oc_cli_endpoint
+  ## Details for accessing and importing the target cluster
+  cluster_name        = var.cluster_name
+  cluster_credentials = module.cluster-credentials.credentials_jsonfile
+
+  ## Access to optional bastion host
+  bastion_host        = var.bastion_host
+  bastion_user        = var.bastion_user
+  bastion_private_key = var.bastion_private_key
+  bastion_port        = var.bastion_port
+  bastion_host_key    = var.bastion_host_key
+  bastion_password    = var.bastion_password
+}
+
---
+++ terraform100/other/mcm-klusterlet/main.tf
@@ -0,0 +1,37 @@
+resource "random_string" "random-dir" {
+  length  = 8
+  special = false
+}
+
+locals {
+  work_directory = "mcm${random_string.random-dir.result}"
+}
+
+module "cluster-import" {
+  source  = "git::https://github.com/IBM-CAMHub-Open/template_rhacm_modules.git//cluster_import?ref=5.2.0"
+
+  dependsOn      = "no dependencies"
+  work_directory = local.work_directory
+
+  ## Details for accessing the RHACM hub-cluster
+  ocp_api_endpoint  = var.ocp_api_endpoint
+  ocp_user          = var.ocp_user
+  ocp_password      = var.ocp_password
+  ocp_ca_cert       = length(var.ocp_ca_cert) > 0 ? base64decode(var.ocp_ca_cert) : var.ocp_ca_cert
+  ocp_token         = var.ocp_token
+  oc_cli_endpoint   = var.oc_cli_endpoint 
+  ## Details for accessing and importing the target cluster
+  cluster_name      = var.cluster_name
+  cluster_endpoint  = var.cluster_endpoint
+  cluster_user      = var.cluster_user
+  cluster_token     = var.cluster_token
+
+  ## Access to optional bastion host
+  bastion_host        = var.bastion_host
+  bastion_user        = var.bastion_user
+  bastion_private_key = var.bastion_private_key
+  bastion_port        = var.bastion_port
+  bastion_host_key    = var.bastion_host_key
+  bastion_password    = var.bastion_password
+}
+
